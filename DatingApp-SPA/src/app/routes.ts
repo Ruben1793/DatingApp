276162c08505ab88914770db8887faea0b2c1662
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-07-28 17:46:28 -0500

    Protecting routes with route guard

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
+    {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
     {path: 'messages', component: MessagesComponent},
     {path: 'lists', component: MessagesComponent},
     {path: '**', redirectTo: 'home', pathMatch: 'full'}
