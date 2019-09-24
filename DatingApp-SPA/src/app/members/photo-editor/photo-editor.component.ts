8f070aa42f6b9fdb4a9d10183e83c0baf7e45ffb
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-09-23 20:06:22 -0500

    Configuring 3rd. party file uploader

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -33,9 +33,24 @@ export class PhotoEditorComponent implements OnInit {
       isHTML5: true,
       allowedFileType: ['image'],
       removeAfterUpload: true,
-      autoUpload: true,
+      autoUpload: false,
       maxFileSize: 10*1024*1024
     });
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false;}
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
 }
