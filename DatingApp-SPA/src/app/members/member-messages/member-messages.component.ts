c1477758a0fbf7f4dd7ce31f9622d3cd20e8187a
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-12-14 17:24:06 -0600

    Getting the message thread in the component

---
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { Message } from 'src/app/_models/message';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+
+  @Input() recipientId: number;
+  messages: Message[];
+
+  constructor(private userService: UserService, private authService: AuthService,
+     private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.userService.getMessageThread(this.authService.decodedtoken.nameid, this.recipientId).subscribe( messages => {
+      this.messages = messages;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
